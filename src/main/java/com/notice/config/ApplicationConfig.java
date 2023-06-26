216107b30a5392c32480c5fbe9a84d7915f5c466
chs1825 <jhsnotnull@gmail.com>
2023-06-26 18:01:09 +0900

    @EnableAspectJAutoProxy 어노테이션 추가

--- src/main/java/com/notice/config/ApplicationConfig.java
+++ src/main/java/com/notice/config/ApplicationConfig.java
@@ -9,7 +9,7 @@ import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @Configuration
 @ComponentScan(basePackages = "com.notice")
-
+@EnableAspectJAutoProxy
 public class ApplicationConfig {
 
     @Bean
